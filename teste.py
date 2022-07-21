bd2471e205d44f1024229fb1949829e32ddf81f1
Vincas2539 <108551966+Vincas2539@users.noreply.github.com>
2022-07-21 15:29:08 -0300

    Tentativa 2

--- teste.py
+++ teste.py
@@ -9,12 +9,11 @@ project.save()
 
 
 SubSet = SubjectSet()
-# s = Subject(106173)
+
 SubSet.links.project = project
 SubSet.display_name = 'Tutorial subject set'
-# SubSet.add_location("C:/Users/alber/Desktop/GitHub/ZooniverseTest/Imagens/Logo Bluemind Só o B Preto.png")
 SubSet.save()
 project.reload()
-print(project.links.SubSet)
+
 
 
--- zooniverse.py
+++ zooniverse.py
@@ -1,4 +1,4 @@
-from panoptes_client import Panoptes, Project, Subject
+from panoptes_client import Panoptes, Project, Subject, SubjectSet
 import os
 
 Panoptes.connect(username='Vincas2539', password='Simagivi@123')
@@ -11,20 +11,28 @@ os.chdir("C:/Users/alber/Desktop/GitHub/ZooniverseTest/Imagens")
 local_files = os.listdir()
 qtd = 0
 try:
+    SubSet = SubjectSet()
+    SubSet.links.project = project
+    SubSet.display_name = 'Tutorial subject set'
+    SubSet.save()
 
     with Subject.async_saves():
         for filename in local_files:
             if filename.endswith(".png" or ".jpg"):
                 qtd += 1
                 s = Subject()
-                s.links.project = project
+                s.links.project = 18892
                 s.add_location(filename)
                 s.save()
+                SubSet.add(s)
+                SubSet.save()
                 print("Uploaded ", qtd)
 
-except:
+    print(SubSet.subjects)
+
+except Exception as Erro:
     print("Error!")
+    print(Erro)
 
 finally:
     print("Finish")
-
